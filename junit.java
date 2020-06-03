3867c0b7b17e2b414337795256aa114d22f382e1
takayu-private <62892066+takayu-private@users.noreply.github.com>
2020-06-03 11:57:17 +0900

    Update junit.java

--- junit.java
+++ junit.java
@@ -8,3 +8,7 @@
     testRuntimeOnly 'org.junit.jupiter:junit-jupiter-engine:5.6.2'
 
 	testRuntimeOnly 'org.junit.vintage:junit-vintage-engine:5.6.2'
+
+    testImplementation "org.mockito:mockito-core:2.28.+"
+    testImplementation 'org.powermock:powermock-module-junit4:2.0.2'
+    testImplementation 'org.powermock:powermock-api-mockito2:2.0.2'
