78c4062a0a70553f8d08a7a4fe797283dbe61cff
takayu-private <62892066+takayu-private@users.noreply.github.com>
2020-06-03 11:50:59 +0900

    Update junit.java

--- junit.java
+++ junit.java
@@ -1,3 +1,10 @@
-created: 2020-05-28T09:16:49.513Z
-nonce: NlvoSSxbhQWcZxpYGIsQiA==
-passwd: bwuXtuYny1cj2zRzFV+tjINLGj8=
+    // Use JUnit4 test framework
+    testImplementation 'junit:junit:4.12'
+
+    // Use JUnit Jupiter API for testing.
+    testImplementation 'org.junit.jupiter:junit-jupiter-api:5.6.2'
+
+    // Use JUnit Jupiter Engine for testing.
+    testRuntimeOnly 'org.junit.jupiter:junit-jupiter-engine:5.6.2'
+
+	testRuntimeOnly 'org.junit.vintage:junit-vintage-engine:5.6.2'
