780bca47b65a25e61f6dd35584794046f724972c
takayu-private <62892066+takayu-private@users.noreply.github.com>
2020-06-05 15:48:37 +0900

    Update junit.java

--- junit.java
+++ junit.java
@@ -1,14 +1,1 @@
-    // Use JUnit4 test framework
-    testImplementation 'junit:junit:4.12'
-
-    // Use JUnit Jupiter API for testing.
-    testImplementation 'org.junit.jupiter:junit-jupiter-api:5.6.2'
-
-    // Use JUnit Jupiter Engine for testing.
-    testRuntimeOnly 'org.junit.jupiter:junit-jupiter-engine:5.6.2'
-
-	testRuntimeOnly 'org.junit.vintage:junit-vintage-engine:5.6.2'
-
-    testImplementation "org.mockito:mockito-core:2.28.+"
-    testImplementation 'org.powermock:powermock-module-junit4:2.0.2'
-    testImplementation 'org.powermock:powermock-api-mockito2:2.0.2'
+http://hc.apache.org/httpcomponents-client-4.5.x/httpclient/examples/org/apache/http/examples/client/ClientExecuteProxy.java
