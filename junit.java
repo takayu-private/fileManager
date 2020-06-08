455ef473b4b907c0050d11bc5ba40f22466b9e57
takayu-private <62892066+takayu-private@users.noreply.github.com>
2020-06-08 13:00:47 +0900

    Update junit.java

--- junit.java
+++ junit.java
@@ -1,1 +1,2 @@
-http://hc.apache.org/httpcomponents-client-4.5.x/httpclient/examples/org/apache/http/examples/client/ClientExecuteProxy.java
+private final static String alteaUrl2 = "https://nodea1.test.api.amadeus.com/1ASIUOACPOCU/V1/departure-control/flights";
+private final static String authorization = "1AAuth eyAidXNlcklkIjogIlRydXN0ZWRVU1IiLAkgIm9yZ2FuaXphdGlvbiI6ICIxQSIgfQ==";
