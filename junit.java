576753de79b7cc9b509d6a3989f8ab02c91779a0
takayu-private <62892066+takayu-private@users.noreply.github.com>
2020-06-09 17:16:55 +0900

    Delete junit.java

--- junit.java
+++
@@ -1,2 +0,0 @@
-private final static String alteaUrl2 = "https://nodea1.test.api.amadeus.com/1ASIUOACPOCU/V1/departure-control/flights";
-private final static String authorization = "1AAuth eyAidXNlcklkIjogIlRydXN0ZWRVU1IiLAkgIm9yZ2FuaXphdGlvbiI6ICIxQSIgfQ==";
